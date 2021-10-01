790bb34ec2442e951047d5039747d62bc79193e8
Sherri Winslow <swinslowcoding@gmail.com>
2021-10-01 14:40:10 -0700

    JS: create variables to reference DOM Elements

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// variables to reference DOM Elements
+var startContentEl = document.querySelector("main");
+var questionsEl = document.querySelector("questions");
+var quizDoneEl = document.querySelector("done");
+var timerEl = document.getElementById("timer");
+var startBtn = document.getElementById("start-timer");
